57729f1d1d5b9b6404e238384b210f60501a66b5
ctrl-code <chebomsta@gmail.com>
2022-01-30 21:38:49 +0530

    Created Front-End Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [stateInput, setStateInput] = useState("");
+    const [stateOutput, setStateOutput] = useState("");
+
+    const InputRef = useRef(null);
+
+    useEffect(() => {
+        InputRef.current.focus();
+    }, []);
+
+    const clickConvert = () => {
+        alert("ACTION DEFINED");
+    }
+
+    return (
+        <div className={styles.page}>
+
+            {/* Heading */}
+            <div className={styles.headingWrapper}>
+                <div className={styles.heading}>
+                    CSS to CSS-IN-JS
+                </div>
+            </div>
+
+            <div className={styles.textAreaEncloser}>
+
+                {/* Input Field */}
+                <textarea className={styles.inputWrapper}
+                    ref={InputRef}
+                    value={stateInput}
+                    onChange={event => {
+                        setStateInput(event.target.value);
+                    }}
+                />
+
+                {/* Output Field */}
+                <textarea className={styles.outputWrapper}
+                    readOnly={true}
+                    value={stateOutput}
+                />
+
+            </div>
+
+            {/* Convert Button */}
+            <div className={styles.buttonWrapper}>
+                <div className={styles.buttonConvert}
+                    onClick={clickConvert}>
+                    CONVERT
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
